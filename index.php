238b14ae0abea0dccee9e2613158ec4b47261ee5
Scott Elwood <mechpants@gmail.com>
2014-03-31 22:00:21 -0700

    Converted everything to php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>CustomsForge Template</title>
+
+  <link rel="stylesheet" href="assets/sass/themes/default/theme.css">
+</head>
+<body>
+  <?php include 'layout/topbar.html'; ?>
+  <?php include 'layout/navigation.html'; ?>
+  
+  <div id="main">
+    <?php $templateName = "templates/{$_GET['page']}.php"; ?>
+    <?php include $templateName; ?>
+  </div>
+
+  <?php include 'layout/footer.html'; ?>
+</body>
+</html>
---
+++ templates/login.php
@@ -0,0 +1,13 @@
+<div id="content">
+  <form class="login" action="">
+    <fieldset>
+      <label>Username</label>
+      <input type="text">
+    </fieldset>
+    <fieldset>
+      <label>Password</label>
+      <input type="password">
+    </fieldset>
+    <button type="submit" class="button submit">Login</button>
+  </form>
+</div>
