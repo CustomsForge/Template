54f40c8695240c293ba2621f722a767078b4acaf
Scott Elwood <mechpants@gmail.com>
2014-03-31 22:07:19 -0700

    Fixing urls for server vs local

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php $root_url = $_SERVER['SERVER_PORT'] == "8888" ? "" : "/template" ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,12 +9,14 @@
   <link rel="stylesheet" href="assets/sass/themes/default/theme.css">
 </head>
 <body>
+
   <?php include 'layout/topbar.html'; ?>
   <?php include 'layout/navigation.html'; ?>
   
   <div id="main">
     <?php $templateName = "templates/{$_GET['page']}.php"; ?>
     <?php include $templateName; ?>
+    <?php echo $_SERVER['SERVER_PORT']; ?>
   </div>
 
   <?php include 'layout/footer.html'; ?>
